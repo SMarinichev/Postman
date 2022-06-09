e6f214456bd3a12122e852bbac1b34a6961d2c07
SMarinichev <ssmarinichev@ya.ru>
2022-06-09 23:54:18 +0300

    fail test

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -14,7 +14,7 @@ public class PostmanTest {
                 .post("/post")
 // Проверки
                 .then()
-                .statusCode(200)
+                .statusCode(201)
                 .body("data", equalTo("Hello"));
         ;
 
